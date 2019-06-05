34fb8aec6cc907a554584d1ca2a80f8aec4676c6
alking <aijingsun6@gmail.com>
2019-06-05 15:11:51 +0800

    update

--- README.md
+++ README.md
@@ -9,12 +9,6 @@ rebar compile
 
 erl -pa ebin
 
-JiebaDic = "dict/jieba.dict.utf8".
-
-ModelDic = "dict/hmm_model.utf8".
-
-ejieba_segment:load_dict(JiebaDic,ModelDic).
-
 L = ejieba_segment:cut( unicode:characters_to_binary("xxx") ).
 
 % L:: [ Bin]
--- src/ejieba_segment.erl
+++ src/ejieba_segment.erl
@@ -1,12 +1,18 @@
 -module(ejieba_segment).
+
 -on_load(init/0).
--export([cut/1,load_dict/2]).
+
+-export([cut/1]).
 
 init() ->
-      ok = erlang:load_nif("priv/ejieba_segment", 0).
+  PrivDir = code:priv_dir(ejieba),
+  ok = erlang:load_nif(filename:join(PrivDir, "ejieba_segment"), 0),
+  JiebaF = filename:join([PrivDir,"dict","jieba.dict.utf8"]),
+  ModF =  filename:join([PrivDir,"dict","hmm_model.utf8"]),
+  ok = load_dict(JiebaF,ModF).
 
 cut(_) ->
-      "NIF library not loaded".
+  "NIF library not loaded".
 
-load_dict(_,_)->
-     "NIF library not loaded".
+load_dict(_, _) ->
+  "NIF library not loaded".
